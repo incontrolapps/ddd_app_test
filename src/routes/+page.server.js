b858e46afe3a8d734ddf290a90fe45b1640cbf4b
Tim Kershaw <ctkershaw@gmail.com>
2025-04-22 16:44:40 +0100

    see if we're getting env

---
+++ src/routes/+page.server.js
@@ -0,0 +1,26 @@
+import { mysqlconnFn } from "$lib/db";
+
+export async function load() {
+  // let mysqlconn = await mysqlconnFn();
+  // try {
+  //   let results = await mysqlconn
+  //     .query(   `SELECT TimePeriod, Value FROM [dbo].[vw_LocalAuthorityMetricsExportFull] 
+  //               WHERE ONSCode = 'FA001'
+  //               AND MetricCode = 'FM001'`)
+  //     .then(function ([rows, fields]) {
+  //       //        console.log(rows);
+  //       return rows;
+  //     });
+
+  //   return {
+  //     data: results,
+  //   };
+  try {
+    return mysqlconnFn
+  }
+   catch (error) {
+    console.error("Got an error!!!");
+    console.log(error);
+    return error;
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 
 <script>
-//export let data;
-//console.log(data);
+export let data;
+console.log(data);
 </script>
 <h1>Testing a database connection</h1>
 
-<!-- {JSON.stringify(data)}
-
+{JSON.stringify(data)}
+<!-- 
 <p>Click state name to see data at bottom.</p>
 {#each data.data as state}
     <button>{state.state}</button>
