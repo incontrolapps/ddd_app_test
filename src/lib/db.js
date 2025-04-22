68c24196e25ee25f2357ef2a51be1182b6aeb594
Tim Kershaw <ctkershaw@gmail.com>
2025-04-22 15:28:37 +0100

    first attempt at DB connection

---
+++ src/lib/db.js
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise";
+import { env } from '$env/dynamic/private';
+
+let mysqlconn = null;
+
+export function mysqlconnFn() {
+  if (!mysqlconn) {
+    mysqlconn = mysql.createConnection({
+      host: env.AZURE_SQL_SERVER,
+      user: env.AZURE_SQL_USER,
+      password: env.AZURE_SQL_PASSWORD,
+      database: env.AZURE_SQL_DATABASE,
+    });
+  }
+
+  return mysqlconn;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+export let data;
+console.log(data);
+</script>
+<h1>Testing a database connection</h1>
+
+{JSON.stringify(data)}
+
+<p>Click state name to see data at bottom.</p>
+{#each data.data as state}
+    <button>{state.state}</button>
+{/each}
+<style>
+button, p {
+    padding: .5em .8em;
+    margin: .5em;
+    font-size: 110%;
+}
+</style>
