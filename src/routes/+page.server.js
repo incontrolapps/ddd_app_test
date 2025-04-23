9895f09660a2c808203f1fce7439b0e6416af66f
Tim Kershaw <ctkershaw@gmail.com>
2025-04-23 16:42:18 +0100

    possible fix

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,23 +1,19 @@
 import { mysqlconnFn } from "$lib/db";
 
 export async function load() {
-  // let mysqlconn = await mysqlconnFn();
-  // try {
-  //   let results = await mysqlconn
-  //     .query(   `SELECT TimePeriod, Value FROM [dbo].[vw_LocalAuthorityMetricsExportFull] 
-  //               WHERE ONSCode = 'FA001'
-  //               AND MetricCode = 'FM001'`)
-  //     .then(function ([rows, fields]) {
-  //       //        console.log(rows);
-  //       return rows;
-  //     });
 
-  //   return {
-  //     data: results,
-  //   };
+  let mysqlconn = await mysqlconnFn();
+
   try {
-    return {data:mysqlconnFn()}
-  }
+    let results = await mysqlconn
+      .query(   `SELECT TimePeriod, Value FROM [dbo].[vw_LocalAuthorityMetricsExportFull] 
+                WHERE ONSCode = 'FA001'
+                AND MetricCode = 'FM001'`)
+      .then(function ([rows, fields]) {
+        //        console.log(rows);
+        return rows;
+      })
+    }
    catch (error) {
     console.error("Got an error!!!");
     console.log(error);
