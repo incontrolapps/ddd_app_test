a306f95dc28ac98a23e705ecd1222b195adf4348
Tim Kershaw <ctkershaw@gmail.com>
2025-04-23 16:09:05 +0100

    added env vars

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,17 +1,17 @@
 import mysql from "mysql2/promise";
 import { env } from '$env/static/private';
 
-//let mysqlconn = null;
+let mysqlconn = null;
 
 export function mysqlconnFn() {
-//   if (!mysqlconn) {
-//     mysqlconn = mysql.createConnection({
-//       host: env.AZURE_SQL_SERVER,
-//       user: env.AZURE_SQL_USER,
-//       password: env.AZURE_SQL_PASSWORD,
-//       database: env.AZURE_SQL_DATABASE
-//     });
-//   }
+ if (!mysqlconn) {
+ mysqlconn = mysql.createConnection({
+host: "sqls-sbx-lgpm-uks-001.database.windows.net",
+user: "CloudSA2846d83b",
+port: 1433,
+database: "sqldb-sbx-lgpm-uks-001"
+});
+}
 
-  return env
+  return mysqlconn
 }
