98316eff9e8183491b1a7d86a9b966346fa09b61
Tim Kershaw <ctkershaw@gmail.com>
2025-04-25 10:44:51 +0100

    added a little formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,11 @@
 
 let { data } = $props();
 console.log('data', data);
+//{SUCCESS: "YEAH !", QUERY: testQuery, RESULT: resultSet.recordset}
 </script>
 <h1>Testing a database connection</h1>
-
-{JSON.stringify(data)}
+<br>
+SUCCESS?: <b>{data.SUCCESS}</b><br>
+QUERY: {data.QUERY}<br>
+RESULT: {JSON.stringify(data.RESULT)}<br>
 
